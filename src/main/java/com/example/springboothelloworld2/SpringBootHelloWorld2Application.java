85e707793c88926900b95cd7bf26433db7dfd805
Ryan Chan <ryan_0206@hotmail.com>
2022-03-16 10:45:37 +0800

    add "Test dev branch" println

--- src/main/java/com/example/springboothelloworld2/SpringBootHelloWorld2Application.java
+++ src/main/java/com/example/springboothelloworld2/SpringBootHelloWorld2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloWorld2Application {
 
     public static void main(String[] args) {
+        System.out.println("Test dev branch");
         SpringApplication.run(SpringBootHelloWorld2Application.class, args);
     }
 
