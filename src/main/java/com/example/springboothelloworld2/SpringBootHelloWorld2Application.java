1759eed2a2d0c323d0bb47e1494c8a93a4d1590a
Ryan Chan <ryan_0206@hotmail.com>
2022-03-16 21:43:42 +0800

    add dev branch log

--- src/main/java/com/example/springboothelloworld2/SpringBootHelloWorld2Application.java
+++ src/main/java/com/example/springboothelloworld2/SpringBootHelloWorld2Application.java
@@ -8,6 +8,7 @@ public class SpringBootHelloWorld2Application {
 
     public static void main(String[] args) {
         System.out.println("Test dev branch");
+        System.out.println("This is dev branch");
         SpringApplication.run(SpringBootHelloWorld2Application.class, args);
     }
 
