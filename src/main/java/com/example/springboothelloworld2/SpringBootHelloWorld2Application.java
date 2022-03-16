f6c4676321787082af35a87e2f7df72a02575728
Ryan Chan <ryan_0206@hotmail.com>
2022-03-16 10:20:54 +0800

    hello world Spring boot

---
+++ src/main/java/com/example/springboothelloworld2/SpringBootHelloWorld2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloworld2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorld2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorld2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboothelloworld2/SpringBootHelloWorld2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloworld2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorld2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
